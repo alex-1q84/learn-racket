8dd1666160215a7142c362c7435481a99ebd29e3
hailxo <hailxl@gmail.com>
2020-04-10 22:00:03 +0800

    添加上下左右四向移动控制支持

--- realm_racket/charpter5/challenges/controllable-ufo.rkt
+++ realm_racket/charpter5/challenges/controllable-ufo.rkt
@@ -12,29 +12,50 @@
 (define UFO-IMAGE (bitmap "../../ufo-1_100px.png"))
 
 ; 一个包含状态和方向的结构，这样就可以把每次的位置和方向存下来
-(struct world (state direction))
+(struct world (state dir))
+(struct posn (x y))
+(struct direction (dir toward))
+
+(define DOWN (direction 'V +))
+(define UP (direction 'V -))
+(define RIGHT (direction 'H +))
+(define LEFT (direction 'H -))
 
 (define (add-3-to-state current-state)
-  (world (+ (world-state current-state)
-            ((world-direction current-state) 3))
-         (world-direction current-state)))
+  (world (move (world-state current-state)
+               (world-dir current-state)
+               3)
+         (world-dir current-state)))
+
+(define (move pos direc step)
+  (if (or (eq? direc LEFT) (eq? direc RIGHT))
+      (posn ((direction-toward direc) (posn-x pos) step)
+            (posn-y pos))
+      (posn (posn-x pos)
+            ((direction-toward direc) (posn-y pos) step))))
 
 (define (set-ufo-direction w key)
   (displayln key)
   (world (world-state w)
          (cond
-           [(key=? key "up") -]
-           [(key=? key "down") +]
-           [else (world-direction w)])))
+           [(key=? key "up") UP]
+           [(key=? key "down") DOWN]
+           [(key=? key "left") LEFT]
+           [(key=? key "right") RIGHT]
+           [else (world-dir w)])))
 
 (define (draw-a-ufo-onto-an-empty-scene current-state)
-  (place-image UFO-IMAGE (/ WIDTH 2) (world-state current-state)
+  (place-image UFO-IMAGE
+               (posn-x (world-state current-state))
+               (posn-y (world-state current-state))
                (empty-scene WIDTH HEIGHT)))
 
 (define (state-is-300 current-state)
-  (>= (world-state current-state) 300))
+  (or (>= (posn-y (world-state current-state)) 300)
+      (>= (posn-x (world-state current-state)) 300)))
 
-(big-bang (world 0 +)
+(big-bang (world (posn (/ WIDTH 2) 0)
+                 DOWN)
   (on-tick add-3-to-state)
   (on-key set-ufo-direction)
   (to-draw draw-a-ufo-onto-an-empty-scene)
