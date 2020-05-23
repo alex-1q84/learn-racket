cf283f4a5e414ee15d3e37163f9df28c2ab5110e
hailxo <hailxl@gmail.com>
2020-05-23 22:18:52 +0800

    add html doc generate and dispatch support

--- system_programming/server.rkt
+++ system_programming/server.rkt
@@ -4,6 +4,8 @@
 
 ;(enter! "lab/racket/system_programming/server.rkt")
 
+(require xml net/url)
+
 (define (server port-no)
   (define main-cust (make-custodian))
   
@@ -33,9 +35,40 @@
             (custodian-shutdown-all cust))))
 
 (define (handle in out)
-  ;Discard the request header (up to blank line):
-  (regexp-match #rx"(\r\n|^)\r\n" in)
-  ;Send reply:
-  (display "HTTP/1.0 200 OK\r\n" out)
-  (display "Server: k\r\nContent-Type: text/html\r\n\r\n" out)
-  (display "<html><body>Hello, world!</body></html>" out))
+  (define req
+    (regexp-match #rx"^GET (.+) HTTP/[0-9]+\\.[0-9]+"
+                  (read-line in)))
+  (when req
+    ;Discard the request header (up to blank line):
+    (regexp-match #rx"(\r\n|^)\r\n" in)
+    (displayln req)
+    ; Dispatch:
+    (let ([xexpr (dispatch (list-ref req 1))])
+      ;Send reply:
+      (display "HTTP/1.0 200 OK\r\n" out)
+      (display "Server: k\r\nContent-Type: text/html\r\n\r\n" out)
+      (display (xexpr->string xexpr) out))))
+
+(define (dispatch str-path)
+  ; Parse the request as url:
+  (define url (string->url str-path))
+  ; Extract the path part:
+  (define path (map path/param-path (url-path url)))
+  ; Find a handler based on the path's first element:
+  (displayln (car path))
+  (define h (hash-ref dispatch-table (car path) #f))
+  (if h
+      ; Call a handler:
+      (h (url-query url))
+      ; No handler found:
+      `(html (head (title "Error"))
+             (body
+              (font ((color "red"))
+                    "Unknown page: "
+                    ,str-path)))))
+
+(define dispatch-table (make-hash))
+
+(hash-set! dispatch-table "hello"
+           (lambda (query)
+             `(html (body "Hello, World!"))))
