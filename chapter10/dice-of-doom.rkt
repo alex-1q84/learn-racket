8458807db1e9ce3da33f8220e1bf8254a7f483d0
hailxo <hailxl@gmail.com>
2020-11-03 20:37:36 +0800

    complete basic image draw methods

--- chapter10/dice-of-doom.rkt
+++ chapter10/dice-of-doom.rkt
@@ -14,7 +14,17 @@
 
 (define TEXT-SIZE 16)
 (define TEXT-COLOR 'black)
-
+(define WIDTH 400)
+(define INFO-X-OFFSET 50)
+(define INFO-Y-OFFSET 50)
+(define FOCUS (text "FOCUS" TEXT-SIZE 'yellow))
+;; FIXME use make-color with transparent
+(define COLORS `('red 'blue))
+(define IMG-LIST (list
+                  (bitmap "graphics/dice1.png")
+                  (bitmap "graphics/dice2.png")
+                  (bitmap "graphics/dice3.png")
+                  (bitmap "graphics/dice4.png")))
 
 
 (define (ISCENE) (empty-scene 300 200))
@@ -30,7 +40,6 @@
 
 
 (define (create-world-of-dice-and-doom)
-  ;; FIXME
   (define board (territory-build))
   (define gamet (game-tree board INIT-PLAYER INIT-SPARE-DICE))
   (define new-world (dice-world #f board gamet))
@@ -51,8 +60,40 @@
 (define (interact-with-board world key)
   ;; FIXME
   (displayln (format "~A key: ~A" interact-with-board key))
+  (cond [(key=? "left" key)
+         (refocus-board world "left")]
+        [(key=? "right" key)
+         (refocus-board world "right")]
+        [(key=? "p" key)
+         (pass world)]
+        [(key=? "\r" key)
+         (mark world)]
+        [(key=? "d" key)
+         (unmark world)]
+        [else world]))
+
+
+(define (refocus-board world direction)
+  ;; FIXME
+  (displayln (format "refocus-board ~A" direction))
   world)
 
+
+(define (pass world)
+  ;; FIXME
+  (displayln "pass world")
+  world)
+
+
+(define (mark world)
+  ;; FIXME
+  (displayln "mark world")
+  world)
+
+
+(define (unmark world)
+  (displayln "unmark world")
+  world)
 
 (define (draw-dice-world world)
   ;; FIXME
@@ -61,18 +102,70 @@
    (add-board-to-scene world (ISCENE))))
 
 
-(define (add-player-info p a-scene)
+(define (add-player-info player a-scene)
   ;; FIXME
-  a-scene)
+  (define str (whose-turn player))
+  (define txt (text str TEXT-SIZE TEXT-COLOR))
+  (place-image txt (- WIDTH INFO-X-OFFSET) INFO-Y-OFFSET a-scene))
+
+
+(define (whose-turn player)
+  (format "Now it's player ~A turn" player))
 
 
 (define (add-board-to-scene world a-scene)
   ;; FIXME
-  a-scene)
+  (define board (dice-world-board world))
+  (define player (game-player (dice-world-gt world)))
+  (define focus? (dice-world-src world))
+  (define trtry1 (first board))
+  (define p-focus (territory-player trtry1))
+  (define t-image (draw-territory trtry1))
+  (define image (draw-focus focus? p-focus player t-image))
+  (define base-s (add-territory trtry1 image a-scene))
+  (for/fold ([s base-s]) ([t (rest board)])
+    (add-territory t (draw-territory t) s)))
+
+
+(define (add-territory t image a-scene)
+  (place-image image (territory-x t) (territory-y t) a-scene))
+
+
+(define (draw-focus marked? p-in-focus p t-image)
+  (if (or (and (not marked?) (= p-in-focus p))
+          (and marked? (not (= p-in-focus p))))
+      (overlay FOCUS t-image)
+      t-image))
+
+
+(define (draw-territory trtry)
+  (define color (color-chooser (territory-player trtry)))
+  (overlay (hexagon color) (draw-dice (territory-dice trtry))))
+
+
+(define (color-chooser n)
+  (list-ref COLORS n))
+
+
+(define (hexagon color)
+  ;; FIXME
+  color)
+
+
+(define (draw-dice n)
+  (define first-dice (get-dice-image 0))
+  (define height-dice (image-height first-dice))
+  (for/fold ([s first-dice]) ([i (- n 1)])
+    (define dice-image (get-dice-image (+ i 1)))
+    (define y-offset (* height-dice (+ .5 (* i .25))))
+    (overlay/offset s 0 y-offset dice-image)))
+
+
+(define (get-dice-image n)
+  (list-ref IMG-LIST (modulo n (length IMG-LIST))))
 
 
 (define (draw-end-of-dice-world world)
-  ;; FIXME
   (define board (dice-world-board world))
   (define message (text (won world) TEXT-SIZE TEXT-COLOR))
   (define background (add-board-to-scene world (PLAIN)))
@@ -89,7 +182,7 @@
   (define player (game-player tree))
   #f
   #;(or (no-more-moves? tree)
-      (for/and ([t board]) (= territory-player t) player)))
+        (for/and ([t board]) (= territory-player t) player)))
 
 
 (define (no-more-moves? tree)
