24d31b1730998960c136138e9bac95f8243548bf
hailxo <hailxl@gmail.com>
2021-02-04 10:04:13 +0800

    day 03 part 2

--- aoc2020/day03.rkt
+++ aoc2020/day03.rkt
@@ -3,22 +3,23 @@
 (struct position (x y) #:transparent)
 (struct worldmap (w h m) #:transparent)
 
-(define (slid posn x-step y-step)
-  (position (+ x-step (position-x posn))
-            (+ y-step (position-y posn))))
+(define (slid right down)
+  (lambda (posn)
+    (position (+ right (position-x posn))
+              (+ down (position-y posn)))))
 
-(define (encounter map posn)
+(define (encounter map posn slid-f)
   (cond
-    [(= (position-y posn) (worldmap-h map)) 0]
+    [(>= (position-y posn) (worldmap-h map)) 0]
     [else
      (+ (tree? map posn)
-        (encounter map (slid posn 3 1)))]))
+        (encounter map (slid-f posn) slid-f))]))
 
 (define (tree? map posn)
   (if (vector-ref (vector-ref (worldmap-m map) (position-y posn))
-              (remainder (position-x posn) (worldmap-w map)))
-       1
-       0))
+                  (remainder (position-x posn) (worldmap-w map)))
+      1
+      0))
 
 (define map
   (call-with-input-file "input03.txt"
@@ -33,7 +34,13 @@
                 (vector-length m)
                 m))))
 
-(encounter map (position 0 0))
+(encounter map (position 0 0) (slid 3 1))
+
+(* (encounter map (position 0 0) (slid 1 1))
+   (encounter map (position 0 0) (slid 3 1))
+   (encounter map (position 0 0) (slid 5 1))
+   (encounter map (position 0 0) (slid 7 1))
+   (encounter map (position 0 0) (slid 1 2)))
 
 (module+ test
   (require rackunit)
