78e54976917035f96ce82da5e0ba1cd36ccc1e53
hailxo <hailxl@gmail.com>
2021-08-27 20:40:21 +0800

    修改成可一次性定义多个 OP 的形式

--- beautiful_racket/stackerizer/stackerizer.rkt
+++ beautiful_racket/stackerizer/stackerizer.rkt
@@ -6,12 +6,24 @@
   #'(#%module-begin
      (for-each displayln (reverse (flatten EXPR)))))
 
-(define-macro (define-op OP)
-  #'(define-macro-cases OP
-      [(OP FIRST) #'FIRST]
-      ; (... ...) 转义 ...
-      [(OP FIRST NEXT (... ...))
-       #'(list 'OP FIRST (OP NEXT (... ...)))]))
+; 一次性定义多个操作符
+(define-macro (define-ops OP ...)
+  #'(begin
+      (define-macro-cases OP ; `OP` from `OP ...`
+        [(OP FIRST) #'FIRST]
+        ; (... ...) 转义 ...
+        [(OP FIRST NEXT (... ...))
+         #'(list 'OP FIRST (OP NEXT (... ...)))])
+      ...)) ; `...` from `OP ...`
 
-(define-op +)
-(define-op *)
+(define-ops + *)
+
+; 上面 `define-ops` 的原理演示
+(define-macro (wrap ARG ...)
+  #'(list '(ARG 42) ...)) ; `...` 会按前面形式自动展开后面剩余参数
+
+(wrap "foo" "bar" "baz")
+
+(define-macro (wrap2 ARG ...)
+  #'(list '(ARG 42 ARG) ...))
+(wrap2 "foo" "bar" "baz")
