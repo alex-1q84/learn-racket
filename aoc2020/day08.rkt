678e81e4a9c99a1ab1d4d9b02c1608c037e2df81
hailxo <hailxl@gmail.com>
2021-12-06 19:59:43 +0800

    complete day08 puzzle 2

--- aoc2020/day08.rkt
+++ aoc2020/day08.rkt
@@ -1,57 +1,26 @@
 #lang racket
-(define *acc* 0)
-(define *pos* 0)
-(define *instr-exec-counts* (make-hash))
-
-(define (next-pos)
-  (set! *pos* (add1 *pos*)))
-
-(define (nop opnum)
-  (next-pos))
-
-(define (acc opnum)
-  (next-pos)
-  (set! *acc* (+ *acc* opnum)))
-
-(define (jmp opnum)
-  (set! *pos* (+ *pos* opnum)))
 
 (define (exec-game-instructions instrs)
   ;需要知道退出状态是正常执行完退出还是指令重复执行而退出
   ; return 'state acc
-  (cond
-    [(empty? instrs) (values 'end *acc*)]
-    [(pos-executed? *pos*) (values 'break *acc*)]
-    [(all-instr-executed? instrs) (values 'end *acc*)]
-    [else
-     (match (instr-at-pos instrs *pos*)
-       [#f *acc*]
-       [(list op opnum)
-        (match op
-          ["nop" (nop opnum)]
-          ["acc" (acc opnum)]
-          ["jmp" (jmp opnum)]
-          [else (raise-argument-error 'exec-game-instructions
-                                      "nop or acc or jmp"
-                                      op)])
-        (count-pos-executions *pos*)
-        (exec-game-instructions instrs)])]))
 
-(define (all-instr-executed? instrs)
-  (eq? (hash-count *instr-exec-counts*)
-       (length instrs)))
+  (let loop ([pos 0]
+             [acc 0]
+             [seen (set)])
+    (define-values (new-pos new-acc)
+      (exec-instr instrs pos acc))
+    (cond
+      [(set-member? seen new-pos) (values 'break acc)]
+      [(eq? new-pos (length instrs)) (values 'end acc)]
+      [else
+       (loop new-pos new-acc (set-add seen new-pos))])))
 
-(define (count-pos-executions pos)
-  (hash-set! *instr-exec-counts* pos
-             (add1 (hash-ref *instr-exec-counts* pos 0))))
 
-(define (pos-executed? pos)
-  (> (hash-ref *instr-exec-counts* pos 0) 1))
-
-(define (instr-at-pos instrs pos)
-  (with-handlers ([exn:fail:contract?
-                   (lambda (e) false)])
-    (list-ref instrs pos)))
+(define (exec-instr instrs pos acc)
+  (match (list-ref instrs pos)
+    [(list "nop" _) (values (add1 pos) acc)]
+    [(list "acc" opnum) (values (add1 pos) (+ acc opnum))]
+    [(list "jmp" opnum) (values (+ pos opnum) acc)]))
 
 (define (patch-instr instrs pos)
   (list-set instrs pos
@@ -59,16 +28,30 @@
               [(list "nop" op) (list "jmp" op)]
               [(list "jmp" op) (list "nop" op)])))
 
-;; TODO
 ; 遍历整个指令集合并找出 nop jmp 指令，逐个测试并找出修改后可以最终执行完的新指令集合
+(define (fix-instrs instrs)
+  (define poses
+    (for/list ([i (in-range (length instrs))]
+               #:when (or (equal? "nop" (first (list-ref instrs i)))
+                          (equal? "jmp" (first (list-ref instrs i)))))
+      i))
+  (for*/or ([pos (in-list poses)]
+            [new-instrs (in-value (patch-instr instrs pos))])
+    (define-values (exit acc)
+      (exec-game-instructions new-instrs))
+    (and (eq? 'end exit)
+         acc)))
 
 (define (parse-to-instructions input)
   (map parse-to-instrion (port->lines input)))
 
 (define (parse-to-instrion str)
   (match str
-    [(regexp #px"(\\w+)\\s+([+-]?\\d+)" (list _ op (app string->number opnum)))
+    [(regexp #px"(acc|nop|jmp)\\s+([+-]?\\d+)" (list _ op (app string->number opnum)))
      (list op opnum)]))
 
 
+;; (exec-game-instructions '(("acc" 1) ("jmp" -1)))
 (exec-game-instructions (parse-to-instructions (open-input-file "input08.txt")))
+
+(fix-instrs (parse-to-instructions (open-input-file "input08.txt")))
