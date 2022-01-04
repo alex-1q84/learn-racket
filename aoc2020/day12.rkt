ee74bf484901a8da37b8d399355d648de2d5e133
hailxo <hailxl@gmail.com>
2022-01-04 17:06:19 +0800

    fix move method bug

--- aoc2020/day12.rkt
+++ aoc2020/day12.rkt
@@ -1,5 +1,6 @@
 #lang racket
 
+(define-struct pos (we sn) #:transparent)
 ;;  +
 ;;  ^
 ;; < > +
@@ -10,48 +11,147 @@
 ;;   - north 90
 ;;   - west 180
 ;;   - south 270
-(define-struct ferry-pos (facing we sn) #:transparent)
+(define-struct (ferry-pos pos) (facing) #:transparent)
 
-(define (move-through pos actions)
+;; we 货轮东西向位置
+;; sn 货轮南北向位置
+;; wp-rel-we 目标位置相对货船东西位置
+;; wp-rel-sn 目标位置相对货船南北位置
+(define-struct ferry-waypoint-pos (ferry-p rel-wp-p) #:transparent)
+
+(define (ferry/move-through pos actions)
   (for/fold ([pos+ pos]) ([action (in-list actions)])
-    (step pos+ action)))
+    (ferry/step pos+ action)))
 
-(define (step pos act)
+(define (ferry/step pos act)
   (define dir (car act))
   (cond
-    [(member dir '(L R)) (turn pos dir (cadr act))]
-    [(member dir '(W E S N F)) (move pos dir (cadr act))]))
+    [(member dir '(L R)) (ferry/rotate pos dir (cadr act))]
+    [(member dir '(W E S N F)) (ferry/move pos dir (cadr act))]))
 
-(define (turn pos dir delta)
+(define (ferry/rotate pos dir degree)
   (match dir
-    ['L (ferry-pos (modulo (- (ferry-pos-facing pos)
-                              (modulo delta 360))
-                           360)
-                   (ferry-pos-we pos)
-                   (ferry-pos-sn pos))]
-    ['R (ferry-pos (modulo (+ (ferry-pos-facing pos)
-                              (modulo delta 360))
-                           360)
-                   (ferry-pos-we pos)
-                   (ferry-pos-sn pos))]))
+    ['L (ferry-pos
+         (pos-we pos)
+         (pos-sn pos)
+         (modulo (- (ferry-pos-facing pos)
+                    (modulo degree 360))
+                 360))]
+    ['R (ferry-pos
+         (pos-we pos)
+         (pos-sn pos)
+         (modulo (+ (ferry-pos-facing pos)
+                    (modulo degree 360))
+                 360))]))
 
-(define (move pos dir delta)
+(define (ferry/move pos dir delta)
   (match dir
     ['W (ferry-pos
-         (ferry-pos-facing pos)
-         (- (ferry-pos-we pos) delta)
-         (ferry-pos-sn pos))]
+         (- (pos-we pos) delta)
+         (pos-sn pos)
+         (ferry-pos-facing pos))]
     ['E (ferry-pos
-         (ferry-pos-facing pos)
-         (+ (ferry-pos-we pos) delta)
-         (ferry-pos-sn pos))]
-    ['S (ferry-pos (ferry-pos-facing pos)
-                   (ferry-pos-we pos)
-                   (- (ferry-pos-sn pos) delta))]
-    ['N (ferry-pos (ferry-pos-facing pos)
-                   (ferry-pos-we pos)
-                   (+ (ferry-pos-sn pos) delta))]
-    ['F (move pos (degree->facing (ferry-pos-facing pos)) delta)]))
+         (+ (pos-we pos) delta)
+         (pos-sn pos)
+         (ferry-pos-facing pos))]
+    ['S (ferry-pos
+         (pos-we pos)
+         (- (pos-sn pos) delta)
+         (ferry-pos-facing pos))]
+    ['N (ferry-pos
+         (pos-we pos)
+         (+ (pos-sn pos) delta)
+         (ferry-pos-facing pos))]
+    ['F (ferry/move pos (degree->facing (ferry-pos-facing pos)) delta)]))
+
+
+;;====================== part 2 ================================
+
+(define (ferry-waypoint/move-through p actions)
+  (for/fold ([pos+ p]) ([action (in-list actions)])
+    (ferry-waypoint/step pos+ action)))
+
+(define (ferry-waypoint/step p act)
+  (define dir (car act))
+  (cond
+    [(member dir '(L R)) (ferry-waypoint-pos
+                          (ferry-waypoint-pos-ferry-p p)
+                          (ferry-waypoint/rotate (ferry-waypoint-pos-rel-wp-p p) dir (cadr act)))]
+    [(member dir '(W E S N F)) (ferry-waypoint/move p dir (cadr act))]))
+
+;; 货船路点整体系统移动计算方法
+(define (ferry-waypoint/move p dir delta)
+  (match dir
+    ['W (ferry-waypoint-pos
+         (ferry-waypoint-pos-ferry-p p)
+         (pos (- (fw/wp-rel-we p) delta)
+              (fw/wp-rel-sn p)))]
+    ['E (ferry-waypoint-pos
+         (ferry-waypoint-pos-ferry-p p)
+         (pos (+ (fw/wp-rel-we p) delta)
+              (fw/wp-rel-sn p)))]
+    ['S (ferry-waypoint-pos
+         (ferry-waypoint-pos-ferry-p p)
+         (pos (fw/wp-rel-we p)
+              (- (fw/wp-rel-sn p) delta)))]
+    ['N (ferry-waypoint-pos
+         (ferry-waypoint-pos-ferry-p p)
+         (pos (fw/wp-rel-we p)
+              (+ (fw/wp-rel-sn p) delta)))]
+    ['F (ferry-waypoint-pos
+         (pos (+ (fw/pos-we p) (* delta (fw/wp-rel-we p)))
+              (+ (fw/pos-sn p) (* delta (fw/wp-rel-sn p))))
+         (ferry-waypoint-pos-rel-wp-p p))]))
+
+(define (fw/pos-we p)
+  (pos-we (ferry-waypoint-pos-ferry-p p)))
+
+(define (fw/pos-sn p)
+  (pos-sn (ferry-waypoint-pos-ferry-p p)))
+
+(define (fw/wp-rel-we p)
+  (pos-we (ferry-waypoint-pos-rel-wp-p p)))
+
+(define (fw/wp-rel-sn p)
+  (pos-sn (ferry-waypoint-pos-rel-wp-p p)))
+
+(define (ferry-waypoint/rotate p dir degree)
+  (define steps (abs (quotient degree 90)))
+  (define r-method (match dir
+                     ['L waypoint/rotate-left]
+                     ['R waypoint/rotate-right]))
+  (for/fold ([new-p p]) ([step (in-range steps)])
+    (r-method new-p)))
+
+(define (waypoint/rotate-left p)
+  (cond
+    [(and (> (pos-we p) 0)
+          (>= (pos-sn p) 0))
+     (pos (- (pos-sn p)) (pos-we p))]
+    [(and (> (pos-we p) 0)
+          (< (pos-sn p) 0))
+     (pos (- (pos-sn p)) (pos-we p))]
+    [(and (< (pos-we p) 0)
+          (>= (pos-sn p) 0))
+     (pos (- (pos-sn p)) (pos-we p))]
+    [(and (< (pos-we p) 0)
+          (< (pos-sn p) 0))
+     (pos (- (pos-sn p)) (pos-we p))]))
+
+(define (waypoint/rotate-right p)
+  (cond
+    [(and (> (pos-we p) 0)
+          (>= (pos-sn p) 0))
+     (pos (pos-sn p) (- (pos-we p)))]
+    [(and (> (pos-we p) 0)
+          (< (pos-sn p) 0))
+     (pos (pos-sn p) (- (pos-we p)))]
+    [(and (< (pos-we p) 0)
+          (>= (pos-sn p) 0))
+     (pos (pos-sn p) (- (pos-we p)))]
+    [(and (< (pos-we p) 0)
+          (< (pos-sn p) 0))
+     (pos (pos-sn p) (pos-we p))]))
 
 (define (degree->facing degree)
   (match degree
@@ -74,11 +174,11 @@
        (list face n)])))
 
 (define (manhattan-distance pos)
-  (+ (abs (ferry-pos-we pos))
-     (abs (ferry-pos-sn pos))))
+  (+ (abs (pos-we pos))
+     (abs (pos-sn pos))))
 
 (let ([actions (list->actions (port->list read-line (open-input-file "input12.txt")))])
-  (manhattan-distance (move-through (ferry-pos 0 0 0) actions)))
+  (manhattan-distance (ferry/move-through (ferry-pos 0 0 0) actions)))
 
 (module+ test
   (require rackunit)
@@ -89,6 +189,39 @@
                                  "F7"
                                  "R90"
                                  "F11"))])
-    (manhattan-distance (move-through (ferry-pos 0 0 0) actions)))
+
+    (ferry-waypoint/move-through (ferry-waypoint-pos (pos 0 0) (pos 10 1)) actions)
+
+    (check-equal? (manhattan-distance (ferry/move-through (ferry-pos 0 0 0) actions)) 25)
+    (check-equal? (manhattan-distance (ferry-waypoint-pos-ferry-p
+                                       (ferry-waypoint/move-through (ferry-waypoint-pos (pos 0 0) (pos 10 1)) actions)))
+                  286))
+
+  (check-equal? (waypoint/rotate-left (pos 3 2)) (pos -2 3))
+  (check-equal? (waypoint/rotate-left (pos 3 -2)) (pos 2 3))
+  (check-equal? (waypoint/rotate-left (pos -3 2)) (pos -2 -3))
+  (check-equal? (waypoint/rotate-left (pos -3 -2)) (pos 2 -3))
+
+  (check-equal? (waypoint/rotate-right (pos 3 2)) (pos 2 -3))
+  (check-equal? (waypoint/rotate-right (pos 3 -2)) (pos -2 -3))
+  (check-equal? (waypoint/rotate-right (pos -3 2)) (pos 2 3))
+  (check-equal? (waypoint/rotate-right (pos -3 -2)) (pos -2 -3))
+
+  (check-equal? (ferry-waypoint/rotate (pos 3 2) 'L 180) (pos -3 -2))
+
+  (check-equal? (ferry-waypoint/move (ferry-waypoint-pos (pos 0 0) (pos 10 1)) 'F 2)
+                (ferry-waypoint-pos (pos 20 2) (pos 10 1)))
+  (check-equal? (ferry-waypoint/move (ferry-waypoint-pos (pos 0 0) (pos 10 1)) 'W 2)
+                (ferry-waypoint-pos (pos 0 0) (pos 8 1)))
+  (check-equal? (ferry-waypoint/move (ferry-waypoint-pos (pos 0 0) (pos 10 1)) 'E 2)
+                (ferry-waypoint-pos (pos 0 0) (pos 12 1)))
+  (check-equal? (ferry-waypoint/move (ferry-waypoint-pos (pos 0 0) (pos 10 1)) 'N 2)
+                (ferry-waypoint-pos (pos 0 0) (pos 10 3)))
+  (check-equal? (ferry-waypoint/move (ferry-waypoint-pos (pos 0 0) (pos 10 1)) 'S 2)
+                (ferry-waypoint-pos (pos 0 0) (pos 10 -1)))
+
+
+  (check-equal? (ferry-waypoint/step (ferry-waypoint-pos (pos 0 0) (pos 10 1)) (list 'S 2))
+                (ferry-waypoint-pos (pos 0 0) (pos 10 -1)))
 
   "all test run")
